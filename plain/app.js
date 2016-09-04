0593cc4a8a00e30b82a36bac9b689815158306eb
DTomoo <tamas.danyi@gmail.com>
2016-09-04 19:06:49 +0200

    introducing time simulation (start/stop/reset)

--- plain/app.js
+++ plain/app.js
@@ -1,5 +1,44 @@
 var app = angular.module('simulationApp', []);
 
-app.controller('mainCtrl', function($scope) {
-    $scope.name = "World";
+app.controller('mainCtrl', function($scope, $timeout) {
+
+$scope.age = 0;
+$scope.displayAge = $scope.age;
+$scope.interval = 100;
+$scope.displayEveryXthStep = 1;
+
+var running = false;
+	
+$scope.start = function() {
+	if (!running) {
+		running = true;
+		nextStep();
+	}
+};
+$scope.stop = function() {
+	if (running) {
+		running = false;
+	};
+};
+$scope.reset = function() {
+	$scope.age = 0;
+};
+
+var nextStep = function() {
+	if (running) {
+		step();
+		$timeout(nextStep, $scope.interval);
+	}
+};
+
+var step = function() {
+	if (running) { 
+		$scope.age += 1;
+	
+		if ($scope.age % $scope.displayEveryXthStep == 0) {
+			$scope.displayAge = $scope.age;
+		}
+	}
+};
+
 });
